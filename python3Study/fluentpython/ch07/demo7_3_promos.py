af3e796db917f50fc8e4a813a1054f7873f5696e
juedui0769 <chixin0769@163.com>
2019-12-22 21:58:53 +0800

    7.3 使用装饰器改进“策略”模式

---
+++ python3Study/fluentpython/ch07/demo7_3_promos.py
@@ -0,0 +1,29 @@
+# 示例7-3
+
+promos = [] #(1)
+
+def promotion(promo_func): #(2)
+    promos.append(promo_func)
+    return promo_func
+
+@promotion #(3)
+def fidelity(order):
+    return order.total() * 0.05 if order.customer.fidelity >= 1000 else 0
+
+@promotion
+def bulk_item(order):
+    discount = 0
+    for item in order.cart:
+        if item.quantity >= 20:
+            discount += item.total() * 0.1
+    return discount
+
+@promotion
+def large_order(order):
+    distinct_items = {item.product for item in order.cart}
+    if len(distinct_items) >= 10:
+        return order.total() * 0.07
+    return 0
+
+def best_promo(order): #(4)
+    return max(promo(order) for promo in promos)
